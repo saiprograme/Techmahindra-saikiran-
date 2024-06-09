ffe27ad6c894d0ee1356e6bca602be186e74b6e4
saiprograme <105115054+saiprograme@users.noreply.github.com>
2024-06-09 21:59:03 +0530

    Add files via upload

---
+++ Task-4/Junit/src/Arithmetic.java
@@ -0,0 +1,14 @@
+
+public class Arithmetic {
+
+
+	//instance method
+	public int product(int number1, int number2) {
+		return number1 * number2;
+		}
+		public int division(int number1, int number2) {
+		return number1 / number2;
+ }
+}
+
+
